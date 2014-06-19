59d42557980a6a46c1f922f041b7c3c9c135c539
Carl Fairclough <carl@mediaburst.co.uk>
2014-06-19 14:52:03 +0100

    Name changes & page load time

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php include_once('backend/index.php') ?>
+<? 
+$time = microtime(); 
+$time = explode(' ', $time); 
+$time = $time[1] + $time[0]; 
+$start = $time; 
+?> 
+
+<?php 
+
+include_once('backend/index.php')
+
+?>
+
+<? 
+$time = microtime(); 
+$time = explode(' ', $time); 
+$time = $time[1] + $time[0]; 
+$finish = $time; 
+$total_time = round(($finish - $start), 4); 
+echo '<p>Page generated in '.$total_time.' seconds.</p>'."\n"; 
+?> 
